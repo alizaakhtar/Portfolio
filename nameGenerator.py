f469f14efad202321709286cb530612247bc38c8
alizaakhtar <aliza.akhtar@hotmail.com>
2017-07-21 09:52:59 -0400

    Add files via upload

---
+++ nameGenerator.py
@@ -0,0 +1,7 @@
+import random
+
+adjectives = ["Alto","Gordo", "Pequeno", "Rapido", "Inteligente", "Amabale", "Gracioso", "Tonto"]
+names = [" Juan", " Juanita", " Pablo", " Guadalupe", " Carlos", " Fernando", " Sophia", " Valentina"]
+
+#for x in range(len(adjectives) or len(names)):
+print(random.choice(adjectives) + random.choice(names))
